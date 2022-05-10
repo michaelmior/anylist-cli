48d44956d2e8b1927085fcefe887fb8c00fb2077
Michael Mior <mmior@cs.rit.edu>
2022-05-10 19:17:03 -0400

    Check that config is written to file

--- src/config.test.js
+++ src/config.test.js
@@ -6,15 +6,17 @@ jest.mock('enquirer', () => {return {prompt: jest.fn()}; });
 const {prompt} = require('enquirer');
 
 const path = require('path');
+const fs = require('fs').promises;
 
 const {getConfig} = require('./config');
+
+const configPath = path.join(process.env.HOME || '', '.anylistrc.json');
 
 test('correct configuration can be loaded from file', async () => {
   // Trigger lazy module loading
   cosmiconfigSync('').search();
 
   // Create a mock configuration
-  const configPath = path.join(process.env.HOME || '', '.anylistrc.json');
   mock({
     [configPath]: '{"email": "foo@example.com", "password": "password"}'
   });
@@ -39,6 +41,10 @@ test('users can be prompted for input with no config file', async () => {
   const config = await getConfig();
   expect(config.email).toBe('bar@example.com');
   expect(config.password).toBe('password2');
+
+  const writtenConfig = JSON.parse(await fs.readFile(configPath, 'utf8'));
+  expect(writtenConfig.email).toBe('bar@example.com');
+  expect(writtenConfig.password).toBe('password2');
 
   mock.restore();
   jest.unmock('enquirer');
